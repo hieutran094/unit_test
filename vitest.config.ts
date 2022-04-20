56926ceebd82f1055a51140a30e6efe1c98edf8e
hieutran094 <hieutran98vn@gmail.com>
2022-04-20 16:08:55 +0700

    remove vite

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import tsPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
-export default defineConfig({
-  plugins: [tsPaths()],
-  test: {
-    globals: true,
-    setupFiles: ['./setupFile.ts'],
-  },
-})
