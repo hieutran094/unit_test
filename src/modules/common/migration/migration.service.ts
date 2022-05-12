7fb3a9a7f1884b2b2a75814ec8ac610be420264b
hieutran094 <hieutran98vn@gmail.com>
2022-05-12 10:22:07 +0700

    test run test when migration

---
+++ src/modules/common/migration/migration.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common'
+import { Connection, getConnection } from 'typeorm'
+
+@Injectable()
+export class MigrationService {
+  async migrateUp() {
+    try {
+      const conn: Connection = getConnection()
+      if (conn) {
+        await conn.runMigrations()
+        console.log(` migrateUp successfully`)
+      } else {
+        throw new HttpException('Invalid connection to ' + conn.name, HttpStatus.BAD_REQUEST)
+      }
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  }
+
+  async migrateDown() {
+    try {
+      const conn: Connection = getConnection()
+      if (conn) {
+        await conn.undoLastMigration()
+        console.log(` migrateDown successfully`)
+      } else {
+        throw new HttpException('Invalid connection to ' + conn.name, HttpStatus.BAD_REQUEST)
+      }
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  }
+}
